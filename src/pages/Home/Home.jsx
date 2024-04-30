fbc5998215bc5e9cb997e4f8f629ce48fb037c7c
antoninosartori <sartoriantonino@gmail.com>
2024-04-30 14:25:48 -0300

    layout and home sections

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import MainLayout from '../../layouts/MainLayout/MainLayout'
+import './Home.css'
+export default function Home() {
+   return (
+      <MainLayout>
+         <section className='Home-main-container'>
+
+            {/* < Hero /> */}
+            {/* < Projects /> */}
+            {/* < Bio /> */}
+            {/* < Fotter /> */}
+
+         </section>
+      </MainLayout>
+   )
+}
