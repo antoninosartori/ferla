a6c3ef6b24ca9d1e0ff8abe6848380aa6c96a850
antoninosartori <sartoriantonino@gmail.com>
2024-04-30 14:27:38 -0300

    Add first component FER-16

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
-import MainLayout from '../../layouts/MainLayout/MainLayout'
 import './Home.css'
+import Hero from '../../features/Hero/Hero'
+import MainLayout from '../../layouts/MainLayout/MainLayout'
+
 export default function Home() {
    return (
       <MainLayout>
          <section className='Home-main-container'>
 
-            {/* < Hero /> */}
+            < Hero />
             {/* < Projects /> */}
             {/* < Bio /> */}
             {/* < Fotter /> */}
