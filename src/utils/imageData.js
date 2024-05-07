34917feefec286007576039330114851ce952411
blonardi <lonardibautista@gmail.com>
2024-05-07 15:02:08 -0300

    FER-9 Issue: add data images for slider

---
+++ src/utils/imageData.js
@@ -0,0 +1,39 @@
+// imageData.js
+
+// Importa las imágenes
+//import {ferla, ferla2, ferla3, ferla4, ferla5} from '../assets/hero';
+import ferla1 from '../assets/hero/ferla1.png'
+import ferla2 from '../assets/hero/ferla2.png'
+import ferla3 from '../assets/hero/ferla3.png'
+import ferla4 from '../assets/hero/ferla4.png'
+import ferla5 from '../assets/hero/ferla5.png'
+
+
+// Define un array con la información de las imágenes
+export const slides = [
+  {
+    id: 1,
+    src: ferla1,
+    alt: 'Descripción de la imagen 1',
+  },
+  {
+    id: 2,
+    src: ferla2,
+    alt: 'Descripción de la imagen 2',
+  },
+  {
+    id: 3,
+    src: ferla3,
+    alt: 'Descripción de la imagen 3',
+  },
+	{
+    id: 4,
+    src: ferla4,
+    alt: 'Descripción de la imagen 4',
+  },
+	{
+    id: 5,
+    src: ferla5,
+    alt: 'Descripción de la imagen 5',
+  },
+];
