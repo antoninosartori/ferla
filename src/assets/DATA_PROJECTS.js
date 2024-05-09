0f22400f78f4ba468636c9c76f8517f7d2aebe42
blonardi <lonardibautista@gmail.com>
2024-05-09 10:53:39 -0300

    FER-15 Issue: Add JSON data projects

---
+++ src/assets/DATA_PROJECTS.js
@@ -0,0 +1,167 @@
+// sarkany, hotel, revolver, venue, tlou, tlou2
+import sarkanyImage from '../assets/projects/sarkany.png'
+import sarkanySound from '../assets/sounds/sarkany/sarkany-musica.mp3'
+
+import hotelImage from '../assets/projects/hotel.jpg'
+import hotelSound from '../assets/sounds/hotel-estudio/hotel-drop-audio.mp3'
+
+import revolverImage from '../assets/projects/revolver.jpg'
+import revolverSound from '../assets/sounds/REVOLVER.mp3'
+
+import venueImage from '../assets/projects/venue_brand.png'
+// venue tiene 2 archivos mp4
+
+import lastofusImage01 from '../assets/projects/lastofus.png'
+import lastofusImage02 from '../assets/images/lastofus02.webp'
+import lastofusISound from '../assets/sounds/last-of-us/I.mp3'
+import lastofusIISound from '../assets/sounds/last-of-us/II.mp3'
+import lastofusIIISound from '../assets/sounds/last-of-us/III.mp3'
+import lastofusIVSound from '../assets/sounds/last-of-us/IV.mp3'
+import lastofusVSound from '../assets/sounds/last-of-us/V.mp3'
+import lastofusVISound from '../assets/sounds/last-of-us/VI.mp3'
+import lastofusVIISound from '../assets/sounds/last-of-us/VII.mp3'
+import lastofusVIIISound from '../assets/sounds/last-of-us/VIII.mp3'
+
+
+export const PROJECTS_DATA = [
+	{
+		id: crypto.randomUUID(),
+		title: "Campaña Sarkany",
+		image: sarkanyImage,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		tracks: [{
+			id: crypto.randomUUID(),
+			name: 'Sarkany Campaña Musica',
+			sound: sarkanySound
+		},]
+	},
+	{	
+		id: crypto.randomUUID(),
+		title: "Hotel estudio",
+		image: hotelImage,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		tracks: [{
+			id: crypto.randomUUID(),
+			name: 'HOTEL DROP RELEASE MUSIK',
+			sound: hotelSound
+		},]
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "REVOLVER",
+		image: revolverImage,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		tracks: [{
+			id: crypto.randomUUID(),
+			name: 'Revolver Sound Design Intro BAF Week',
+			sound: revolverSound
+		},]
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "VENUE BRAND EXPERIENCE",
+		image: venueImage,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		//tracks: [{
+		//	id: crypto.randomUUID(),
+		//	name: 'Revolver Sound Design Intro BAF Week',
+		//	sound: revolverSound
+		//},]
+	},
+  {
+		id: crypto.randomUUID(),
+		title: "The Last of Us Serie",
+		image: lastofusImage01,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		tracks: [
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido I',
+			sound: lastofusISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido II',
+			sound: lastofusIISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido III',
+			sound: lastofusIIISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido IV',
+			sound: lastofusIVSound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido V',
+			sound: lastofusVSound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido VI',
+			sound: lastofusVISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido VII',
+			sound: lastofusVIISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido VIII',
+			sound: lastofusVIIISound
+		}
+	]
+	},
+	{
+		id: crypto.randomUUID(),
+		title: "The Last of Us Serie",
+		image: lastofusImage02,
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		tracks: [
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido I',
+			sound: lastofusISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido II',
+			sound: lastofusIISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido III',
+			sound: lastofusIIISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido IV',
+			sound: lastofusIVSound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido V',
+			sound: lastofusVSound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido VI',
+			sound: lastofusVISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido VII',
+			sound: lastofusVIISound
+		},
+		{
+			id: crypto.randomUUID(),
+			name: 'Sonido VIII',
+			sound: lastofusVIIISound
+		}
+	]
+	}
+]
