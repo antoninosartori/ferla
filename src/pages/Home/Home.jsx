305feb0b7eff9f5f4aadf816baccba7d0fda8bbb
antoninosartori <sartoriantonino@gmail.com>
2024-05-09 13:02:25 -0300

    Add button to toggle hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,28 @@
 import './Home.css'
 import Hero from '../../sections/Hero/Hero'
+import Projects from '../Projects/Projects'
+import { useState } from 'react'
 export default function Home() {
+   const [isSliderHero, setIsSliderHero] = useState(true)
+
 	return (
 		<section className='Home-main-container'>
-
-			< Hero />
-			{/* < Projects /> */}
+         <button style={{
+            position: 'absolute', 
+            top: '30px', 
+            left: '30px', 
+            backgroundColor: 'var(--color-primary)',
+            outline: 'none',
+            border: 'none',
+            padding: '5px',
+            borderRadius: '5px',
+            fontWeight: 'bold',
+            fontFamily: 'var(--font-primary)',
+            color: 'var(--color-logo)',
+            cursor: 'pointer'
+            }} onClick={() => setIsSliderHero(!isSliderHero)}>Cambiar HERO</button>
+			{isSliderHero ? < Hero /> : <></>}
+			< Projects />
 			{/* < Bio /> */}
 			{/* < Fotter /> */}
 
